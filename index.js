bcf82e120cc53676b1f36dfe7fa6bddd11f2c158
Trac Nguyen <nguyendtrac@gmail.com>
2018-10-26 06:30:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function printBadges(array) {
+  for (let counter = 0; counter < array.length; counter++) {
+    console.log(`Welcome ${array[counter]}! You are employee #${counter+1}.`);
+  }
+  return array
+}
+
+function tailsNeverFails() {
+  let tails = 0;
+
+  while (Math.random() >= 0.5) {
+    tails++;
+  }
+
+  return `You got ${tails} tails in a row!`;
+}
